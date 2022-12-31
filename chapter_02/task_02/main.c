7cb4d28a41f9bee92aec63f13604755b37e909b2
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-31 20:56:03 +0300

    Добавил решения практической части для второй главы

---
+++ chapter_02/task_02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * \brief Напишите программу, выводящую ваше имя и адрес.
+ *
+ * \return int
+ */
+int main(void)
+{
+    printf("Мои имя и фамилия\n");
+    printf("Мой адрес\n");
+
+    return 0;
+}
