cab1d6d69299421400d1264b9865502e6c18e44b
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-01-14 16:34:19 +0300

    Добавил листинг 3.4 к главе 3

---
+++ chapter_03/listing_04/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * @brief Дополнительные свойства функции printf()
+ *
+ * @return int
+ */
+int main(void)
+{
+    unsigned int un = 3000000000; /* система с 32-битным типом int */
+    short end = 200; /* и 16-битным типом short */
+    long big = 65537;
+    long long verybig = 12345678908642llu;
+    printf("un = %u, но не %d\n", un, un);
+    printf("end = %hd и %d\n", end, end);
+    printf("big = %ld, но не %hd\n", big, big);
+    printf("verybig = %lld, но не %ld\n", verybig, verybig);
+    return 0;
+}
