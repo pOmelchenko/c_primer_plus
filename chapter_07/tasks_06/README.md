b96f16e3c0c143252004d3bd8678a3e0d78263b2
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-07-31 18:38:12 +0300

    Добавил оформление к заданию 6 в главе 7

---
+++ chapter_07/tasks_06/README.md
@@ -0,0 +1,5 @@
+# Задание 7.6
+
+```shell
+gcc main.c -o main
+```
---
+++ chapter_07/tasks_06/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * @brief Упражнение по программированию 7.6
+ *
+ * @details Add task details!
+ *
+ * @return int
+ */
+int main(void)
+{
+    /** @todo write your code here */
+
+    return 0;
+}
