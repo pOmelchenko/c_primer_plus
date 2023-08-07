8229cca86e9ba8dfa49ef3df8bf00d8954e5cb53
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-08-07 23:16:32 +0300

    Добавил решение к заданию 7 в главе 7

--- chapter_07/tasks_07/main.c
+++ chapter_07/tasks_07/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define NORMAL_WORK_TIME 40
+#define NORMAL_TARIFF_RATE 10
+#define OVERTIME_COEFFICIENT 1.5
+
 /**
  * @brief Упражнение по программированию 7.7
  *
@@ -22,7 +26,38 @@
  */
 int main(void)
 {
-    /** @todo write your code here */
+    int hours;
+    float earned, tax;
+
+
+    printf("Введите количество отработанных часов: ");
+    scanf("%d", &hours);
+
+    if (hours <= 40) {
+        earned = hours * NORMAL_TARIFF_RATE;
+    } else {
+        float normal_earned = (float) NORMAL_WORK_TIME * NORMAL_TARIFF_RATE;
+        float overtime_earned = (float) (hours - NORMAL_WORK_TIME) * (NORMAL_TARIFF_RATE * OVERTIME_COEFFICIENT);
+
+        earned = normal_earned + overtime_earned;
+    }
+
+    if (earned <= 300) {
+        tax = earned * 15 / 100;
+    } else if (earned <= 450) {
+        float second_part = earned - 300;
+
+        tax = 300 * 15 / 100;
+        tax += second_part * 20 / 100;
+    } else {
+        float third_part = earned - 300 - 150;
+
+        tax = 300 * 15 / 100;
+        tax += 150 * 20 / 100;
+        tax += third_part * 25 / 100;
+    }
+
+    printf("Начислено: %.2f\nНалог %.2f\nЧистый доход: %.2f\n", earned, tax, earned - tax);
 
     return 0;
 }
