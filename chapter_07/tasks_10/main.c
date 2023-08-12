8e4d84be9aa52dbe6d35f2885494c63925695a8e
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-08-12 19:47:44 +0300

    Добавил решение к заданию 10 в главе 7

--- chapter_07/tasks_10/main.c
+++ chapter_07/tasks_10/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define TAX_CATEGORY_SINGLE 17850
+#define TAX_CATEGORY_MARRIAGE 23900
+#define TAX_CATEGORY_MARRIAGE_AND_JOINT 29750
+#define TAX_CATEGORY_MARRIAGE_AND_SEPARATE 14875
+
 /**
  * @brief Упражнение по программированию 7.10
  *
@@ -38,7 +43,63 @@
  */
 int main(void)
 {
-    /** @todo write your code here */
+    float income = 0.0, tax_amount = 0.0, income_over_the_limit = 0.0;
+    int select;
+
+    printf("Введите облагаемый налогом доход: ");
+    scanf("%f", &income);
+
+    printf("Выберите один из вариантов налогообложения: ");
+    while (scanf("%d", &select) == 1) {
+        switch (select) {
+            case 1:
+                printf("Выбран вариант: Одинокий\n");
+
+                if (income <= TAX_CATEGORY_SINGLE) {
+                    tax_amount = income * 0.15;
+                } else {
+                    tax_amount = TAX_CATEGORY_SINGLE * 0.15;
+                    income_over_the_limit = income - TAX_CATEGORY_SINGLE;
+                }
+                break;
+            case 2:
+                printf("Выбран вариант: Глава семейства\n");
+
+                if (income <= TAX_CATEGORY_MARRIAGE) {
+                    tax_amount = income * 0.15;
+                } else {
+                    tax_amount = TAX_CATEGORY_MARRIAGE * 0.15;
+                    income_over_the_limit = income - TAX_CATEGORY_MARRIAGE;
+                }
+                break;
+            case 3:
+                printf("Выбран вариант: Состоит в браке, совместное ведение хозяйства\n");
+
+                if (income <= TAX_CATEGORY_MARRIAGE_AND_JOINT) {
+                    tax_amount = income * 0.15;
+                } else {
+                    tax_amount = TAX_CATEGORY_MARRIAGE_AND_JOINT * 0.15;
+                    income_over_the_limit = income - TAX_CATEGORY_MARRIAGE_AND_JOINT;
+                }
+                break;
+            case 4:
+                printf("Выбран вариант: Состоит в браке, раздельное ведение хозяйства\n");
+
+                if (income <= TAX_CATEGORY_MARRIAGE_AND_SEPARATE) {
+                    tax_amount = income * 0.15;
+                } else {
+                    tax_amount = TAX_CATEGORY_MARRIAGE_AND_SEPARATE * 0.15;
+                    income_over_the_limit = income - TAX_CATEGORY_MARRIAGE_AND_SEPARATE;
+                }
+                break;
+        }
+
+        tax_amount += income_over_the_limit * 0.28;
+
+        printf("Размер налога: %0.2f\n", tax_amount);
+
+        printf("Выберите один из вариантов налогообложения: ");
+    }
 
     return 0;
 }
