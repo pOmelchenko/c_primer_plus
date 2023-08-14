bb3c473133c1503bbd44f4d00a2de9dc3c8db503
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-08-14 22:52:51 +0300

    Исправил код для получения решения

--- chapter_07/self_tests/task_10/main.c
+++ chapter_07/self_tests/task_10/main.c
@@ -9,17 +9,19 @@ int main(void)
 {
     char ch;
     while ((ch = getchar()) != '#') {
-        if (ch == '\n')
-            continue;
-        printf("Шаг 1\n");
-        if (ch == 'c')
-            continue;
-        else if (ch == 'b')
-            break;
-        else if (ch == 'h')
-            goto laststep;
-        printf("Шаг 2\n");
-        laststep: printf("Шаг 3\n");
+        if (ch != '\n') {
+            printf("Шаг 1\n");
+            if (ch != 'c') {
+                if (ch == 'b')
+                    break;
+                else if (ch == 'h')
+                    printf("Шаг 3\n");
+                else {
+                    printf("Шаг 2\n");
+                    printf("Шаг 3\n");
+                }
+            }
+        }
     }
     printf("Готово\n");
     return 0;
