87c6a1589c232c91b3c108f7762cb5f014801686
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-19 17:36:09 +0300

    Добавил решение к заданию 6 в главе 8

--- chapter_08/tasks_06/main.c
+++ chapter_08/tasks_06/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-char get_choice(void);
 char get_first(void);
-int get_int(void);
-void count(void);
 
 /**
  * @brief Упражнение по программированию 8.6
@@ -14,72 +11,22 @@ void count(void);
  */
 int main(void)
 {
-    int choice;
-    void count(void);
-    while ((choice = get_choice()) != 'q') {
-        switch (choice) {
-            case 'a':
-                printf("Покупайте дешево, продавайте дорого\n");
-                break;
-            case 'b':
-                printf("\a"); /* ANSI */
-                break;
-            case 'c':
-                count();
-                break;
-            default:
-                printf("Ошибка!\n");
-                break;
-        }
-    }
+    char ch;
+    ch = get_first();
+
+    printf("Результат: %c\n", ch);
     printf("Программа завершена.\n");
     return 0;
-}
-
-void count(void)
-{
-    int n, i;
-    printf("До какого предела вести счет? Введите целое число:\n");
-    n = get_int();
-    for (i = 1; i <= n; i++)
-        printf("%d\n", i);
-    while (getchar() != '\n')
-        continue;
-}
-
-char get_choice(void)
-{
-    int ch;
-    printf("Введите букву, соответствующую выбранному варианту:\n");
-    printf("a. совет          c. звонок\n");
-    printf("b. подсчет        q. выход\n");
-    ch = get_first();
-    while (ch != 'a' && ch != 'b' && ch != 'c' && ch != 'q') {
-        printf("Выберите a, b, c или q.\n");
-        ch = get_first();
-    }
-    return ch;
 }
 
 char get_first(void)
 {
     int ch;
-    ch = getchar();
+
+    while ((ch = getchar()) == ' ')
+        continue;
     while (getchar() != '\n')
         continue;
-    return ch;
-}
 
-int get_int(void)
-{
-    int input;
-    char ch;
-    while (scanf("%d", &input) != 1)
-    {
-        while ((ch = getchar()) != '\n')
-            putchar(ch);  // отбросить неправильный ввод
-        printf(" не является целочисленным.\n");
-        printf("Введите целое число, такое как 25, -178 или 3:\n");
-    }
-    return input;
+    return ch;
 }
