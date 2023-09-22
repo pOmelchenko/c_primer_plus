992c7195919f5142e50ea705a04f3353760d4791
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-22 21:36:54 +0300

    Добавил оформление к заданию 1 в главе 9

---
+++ chapter_09/tasks_01/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @brief Упражнение по программированию 9.1
+ *
+ * @details Напишите функцию по имени <code>man(x,y)</code>, которая возвращает меньшее из двух значений
+ * <code>double</code>. Протестируйте эту функцию с помощью простого драйвера.
+ *
+ * @return int
+ */
+int main(void)
+{
+    /** @todo write your code here */
+
+    return 0;
+}
