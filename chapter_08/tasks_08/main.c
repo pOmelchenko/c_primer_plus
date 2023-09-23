9e26fa462c6f360d0c890927fc85129933bd6caf
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-23 10:57:06 +0300

    Добавил решение к заданию 8 в главе 8

--- chapter_08/tasks_08/main.c
+++ chapter_08/tasks_08/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 /**
+ * @brief Вывод на экран меню с ожиданием выбора от пользователя
+ *
+ * @return
+ */
+char get_choice(void);
+
+/**
+ * @brief Получение одного символа от пользователя
+ *
+ * @return
+ */
+char get_first(void);
+
+/**
+ * @brief Получение числа от пользователя
+ *
+ * @return
+ */
+double get_number(void);
+
+/**
  * @brief Упражнение по программированию 8.8
  *
  * @details Напишите программу, которая выводит на экран меню, предлагающее выбрать сложение, вычитание, умножение или
@@ -39,7 +60,87 @@
  */
 int main(void)
 {
-    /** @todo write your code here */
+    int choice;
+    double num_a, num_b;
+
+    while ((choice = get_choice()) != 'q') {
+        printf("Введите первое число: ");
+        num_a = get_number();
+
+        printf("Введите второе число: ");
+        num_b = get_number();
+
+        switch (choice) {
+            case 'a':
+                printf("%0.1f + %0.1f = %0.1f\n", num_a, num_b, num_a + num_b);
+                break;
+            case 's':
+                printf("%0.1f - %0.1f = %0.1f\n", num_a, num_b, num_a - num_b);
+                break;
+            case 'm':
+                printf("%0.1f * %0.1f = %0.1f\n", num_a, num_b, num_a * num_b);
+                break;
+            case 'd':
+                if (0 == num_b) {
+                    printf("Введите число отличное от 0: ");
+                    num_b = get_number();
+                }
+                printf("%0.1f / %0.1f = %0.1f\n", num_a, num_b, num_a / num_b);
+                break;
+            default:
+                break;
+        }
+    }
+
+    printf("Программа завершена.\n");
 
     return 0;
+}
+
+char get_choice(void)
+{
+    char choice;
+
+    printf("Выберите операцию из списка:\n");
+    printf("a. сложение            s. вычитание\n");
+    printf("m. умножение           d. деление\n");
+    printf("q. завершение\n");
+
+    choice = get_first();
+
+    if (choice != 'a' && choice != 's' && choice != 'm' && choice != 'd' && choice != 'q') {
+        printf("Выбран неверный вариант, попробуйте еще раз\n");
+        choice = get_first();
+    }
+
+    return choice;
+}
+
+char get_first(void)
+{
+    char input;
+
+    while ((input = getchar()) == ' ')
+        continue;
+    while (getchar() != '\n')
+        continue;
+
+    return input;
+}
+
+double get_number(void)
+{
+    double number;
+
+    while (scanf("%lf", &number) != 1) {
+        while(getchar() != '\n')
+            continue;
+
+        printf("Введите число, такое как 2.5, -1.78E8, или 3: ");
+    }
+
+    while(getchar() != '\n')
+        continue;
+
+    return number;
 }
