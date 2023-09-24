67ba4a40a433b2ee8af81624526b0188c1696bad
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-24 08:48:00 +0300

    Добавил оформление к заданию 2 в главе 9

---
+++ chapter_09/tasks_02/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @brief Упражнение по программированию 9.2
+ *
+ * @details Напишите функцию по имени <code>chline(ch, i, j)</code>, которая выводит требуемый символ в столбцах с
+ * <code>i</code> по <code>j</code>. Протестируйте эту функцию с помощью простого драйвера.
+ *
+ * @return int
+ */
+int main(void)
+{
+    /** @todo write your code here */
+
+    return 0;
+}
