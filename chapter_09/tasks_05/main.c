78db9270c7eefc95a41f713f6cf8eb4549f7992c
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-27 23:41:30 +0300

    Добавил оформление к заданию 5 в главе 9

---
+++ chapter_09/tasks_05/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * @brief Упражнение по программированию 9.5
+ *
+ * @details Напишите и протестируйте функцию по имени <code>large_of()</code>, которая заменяет содержимое двух
+ * переменных <code>double</code> большим из значений. Например, вызов <code>large_of(x,y)</code> присвоит переменным
+ * <code>x</code> и <code>y</code> большее из их значений.
+ *
+ * @return int
+ */
+int main(void)
+{
+    /** @todo write your code here */
+
+    return 0;
+}
