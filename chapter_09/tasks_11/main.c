02eda103e776a75dae2a7b2193c2cff46e38c2ee
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-01 13:50:41 +0300

    Добавил оформление к заданию 11 в главе 9

---
+++ chapter_09/tasks_11/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @brief Упражнение по программированию 9.11
+ *
+ * @details Напишите и протестируйте функцию <code>Fibonacci()</code>, в которой для вычисления чисел Фибоначчи вместо
+ * рекурсии применяется цикл.
+ *
+ * @return int
+ */
+int main(void)
+{
+    /** @todo write your code here */
+
+    return 0;
+}
