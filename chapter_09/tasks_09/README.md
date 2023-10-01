b4089da5b0891d07aaf949ad24496db49cf8cca3
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-01 13:41:56 +0300

    Добавил оформление к заданию 9 в главе 9

---
+++ chapter_09/tasks_09/README.md
@@ -0,0 +1,5 @@
+# Задание 9.9
+
+```shell
+gcc main.c -o main
+```
---
+++ chapter_09/tasks_09/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * @brief Упражнение по программированию 9.9
+ *
+ * @details Еще раз выполните упражнение 8, но на этот раз используйте рекурсивную функцию.
+ *
+ * @return int
+ */
+int main(void)
+{
+    /** @todo write your code here */
+
+    return 0;
+}
