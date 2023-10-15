75f91b1577a9abc1f3f2ba4dfb734058a28768f8
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-16 00:23:31 +0300

    Добавил решение к заданию 1 в главе 9

--- chapter_09/tasks_01/main.c
+++ chapter_09/tasks_01/main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 
+double min(double, double);
+
 /**
  * @brief Упражнение по программированию 9.1
  *
- * @details Напишите функцию по имени <code>man(x,y)</code>, которая возвращает меньшее из двух значений
+ * @details Напишите функцию по имени <code>min(x,y)</code>, которая возвращает меньшее из двух значений
  * <code>double</code>. Протестируйте эту функцию с помощью простого драйвера.
  *
  * @return int
  */
 int main(void)
 {
-    /** @todo write your code here */
+    double a, b;
+
+    a = 2.2;
+    b = 1.2;
+
+    printf("a = %0.2f, b = %0.2f, res = %0.2f\n", a, b, min(a, b));
+
+    a = 2.2;
+    b = 3.2;
+
+    printf("a = %0.2f, b = %0.2f, res = %0.2f\n", a, b, min(a, b));
 
     return 0;
+}
+
+double min(double a, double b)
+{
+    return a < b ? a : b;
 }
