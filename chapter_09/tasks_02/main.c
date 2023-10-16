a1b5f921ec35558c97c9c41b8bef01bdb004c58d
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-16 09:10:00 +0300

    Добавил решение к заданию 2 в главе 9

--- chapter_09/tasks_02/main.c
+++ chapter_09/tasks_02/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 /**
+ * @brief Функция которая выводит матрицу из переданных символов
+ */
+
+/**
+ * @brief Функция которая выводит матрицу из переданных символов
+ * @param ch символ в матрице
+ * @param i количество столбцов
+ * @param j количество строк
+ */
+void chline(char ch, int i, int j);
+
+/**
  * @brief Упражнение по программированию 9.2
  *
  * @details Напишите функцию по имени <code>chline(ch, i, j)</code>, которая выводит требуемый символ в столбцах с
@@ -10,7 +22,23 @@
  */
 int main(void)
 {
-    /** @todo write your code here */
+    chline('a', 3, 3);
+
+    chline('b', 3, 6);
+
+    chline('c', 6, 3);
 
     return 0;
+}
+
+void chline(char ch, int i, int j)
+{
+    int x, y;
+
+    for (y = 0; y < j; y++) {
+        for (x = 0; x < i; x++) {
+            putchar(ch);
+        }
+        putchar('\n');
+    }
 }
