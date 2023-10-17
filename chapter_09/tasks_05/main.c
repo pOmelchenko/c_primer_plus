29379bc962a8126ca0ae744374fd32a3bb0892b4
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-18 00:23:06 +0300

    Добавил решение к заданию 5 в главе 9

--- chapter_09/tasks_05/main.c
+++ chapter_09/tasks_05/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 /**
+ * @brief замена обоих числе большим из переданных
+ *
+ * @param a первое число
+ * @param b второе число
+ */
+void large_of(double * a, double * b);
+
+/**
  * @brief Упражнение по программированию 9.5
  *
  * @details Напишите и протестируйте функцию по имени <code>large_of()</code>, которая заменяет содержимое двух
@@ -11,7 +19,19 @@
  */
 int main(void)
 {
-    /** @todo write your code here */
+    double a, b;
+
+    a = 10.1;
+    b = 3.2;
+
+    large_of(&a, &b);
+
+    printf("Result a = %0.2f, b = %0.2f", a, b);
 
     return 0;
+}
+
+void large_of(double * a, double * b)
+{
+    *a = *b = (*a > *b ? *a : *b);
 }
